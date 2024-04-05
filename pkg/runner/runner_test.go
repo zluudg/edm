1c849c40d1bae0077c810d79b48b4e05052737aa
Patrik Lundin <patlu@sunet.se>
2024-04-05 15:52:03 +0200

    Add some tests
    Verify uint{32,64} data returned from ipBytesToInt() and ip6BytesToInt()
    can be successfully used to reconstruct the original addresses.

--- pkg/runner/runner_test.go
+++ pkg/runner/runner_test.go
@@ -1,6 +1,7 @@
 package runner
 
 import (
+	"encoding/binary"
 	"net/netip"
 	"os"
 	"strings"
@@ -98,5 +99,62 @@ func TestDTMStatusBitsUnknown(t *testing.T) {
 
 	if !strings.HasPrefix(dsb.String(), "unknown flags in status: ") {
 		t.Fatalf("have: %s, want prefix: %s", dsb.String(), expectedString)
+	}
+}
+
+func TestDTMIPBytesToInt(t *testing.T) {
+
+	ipv4AddrString := "198.51.100.15"
+
+	ip4Addr, err := netip.ParseAddr(ipv4AddrString)
+	if err != nil {
+		t.Fatalf("unable to parse IPv4 test address '%s': %s", ipv4AddrString, err)
+	}
+
+	ip4Int, err := ipBytesToInt(ip4Addr.AsSlice())
+	if err != nil {
+		t.Fatalf("unable to create uint32 variable from IPv4 test address '%s': %s", ipv4AddrString, err)
+	}
+
+	// Go back to IPv4 data
+	constructedV4Data := []byte{}
+	constructedV4Data = binary.BigEndian.AppendUint32(constructedV4Data, ip4Int)
+
+	constructedIp4Addr, ok := netip.AddrFromSlice(constructedV4Data)
+	if !ok {
+		t.Fatalf("unable to create netip from from constructed IPv4 bytes: %b", constructedV4Data)
+	}
+
+	if ip4Addr != constructedIp4Addr {
+		t.Fatalf("have: %s, want: %s", constructedIp4Addr, ip4Addr)
+	}
+}
+
+func TestDTMIP6BytesToInt(t *testing.T) {
+
+	ipv6AddrString := "2001:db8:1122:3344:5566:7788:99aa:bbcc"
+
+	ip6Addr, err := netip.ParseAddr(ipv6AddrString)
+	if err != nil {
+		t.Fatalf("unable to parse IPv6 test address '%s': %s", ipv6AddrString, err)
+	}
+
+	ip6Network, ip6Host, err := ip6BytesToInt(ip6Addr.AsSlice())
+	if err != nil {
+		t.Fatalf("unable to create uint64 variables from IPv6 test address '%s': %s", ipv6AddrString, err)
+	}
+
+	// Go back to complete IPv6 data
+	constructedV6Data := []byte{}
+	constructedV6Data = binary.BigEndian.AppendUint64(constructedV6Data, ip6Network)
+	constructedV6Data = binary.BigEndian.AppendUint64(constructedV6Data, ip6Host)
+
+	constructedIp6Addr, ok := netip.AddrFromSlice(constructedV6Data)
+	if !ok {
+		t.Fatalf("unable to create netip from from constructed IPv6 bytes: %b", constructedV6Data)
+	}
+
+	if ip6Addr != constructedIp6Addr {
+		t.Fatalf("have: %s, want: %s", constructedIp6Addr, ip6Addr)
 	}
 }
