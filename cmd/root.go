24e62fcaefb9baef445284e155783b9e1de9ef66
Patrik Lundin <patlu@sunet.se>
2024-09-30 10:18:24 +0200

    More flag cleanup
    The --config flag also takes a filename, so it is --config-file now.
    
    Also fix config output to mention TOML files. After the switch from
    manual handling to viper edm can actually read multiple types of config
    formats, but TOML is what has been used up to this point, so mention it
    specifically.

--- cmd/root.go
+++ cmd/root.go
@@ -40,7 +40,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file for sensitive information (default is $HOME/.edm.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config-file", "", "config file for sensitive information (default is $HOME/.edm.toml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
@@ -59,7 +59,7 @@ func initConfig() {
 
 		// Search config in home directory with name ".edm" (without extension).
 		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
+		viper.SetConfigType("toml")
 		viper.SetConfigName(".edm")
 	}
 
